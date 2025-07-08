703bc0f4d813429782b6e3414266d785927afd4f
Kier Petersen <qlikpetersen@google.com>
2025-07-08 14:52:45 -0600

    Installer for Windows

---
+++ extraStuff/install-gitbash.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+which pyenv >/dev/null
+if ([ $? = 1 ]); then     
+  echo Please install pyenv before installing this.
+  exit 1
+fi
+
+pyenv install 3.12.9
+pyenv global 3.12.9
+
+python -m pip install -U pip
+pip install uv
+cd ~
+git clone https://github.com/comfyanonymous/ComfyUI ComfyUI
+cd ~/ComfyUI
+
+uv venv
+uv pip install pip comfy-cli 
+uv run comfy set-default .
+uv run comfy install --restore
+
+cd custom_nodes
+git clone https://github.com/qlikpetersen/ComfyUI-AI_Tools
+cd ..
+
+cp custom_nodes/ComfyUI-AI_Tools/extraStuff/run.* .
+chmod 755 run.sh
+
+uv pip install -r custom_nodes/ComfyUI-AI_Tools/requirements.txt
+
